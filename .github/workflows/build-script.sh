ffd55939fdfc9da89c134b2ce7866af5b7fc7ce5
Talha Masoud <talha.masoud@hotmail.com>
2023-11-29 13:25:46 +0500

    Update build-script.sh

--- .github/workflows/build-script.sh
+++ .github/workflows/build-script.sh
@@ -3,9 +3,6 @@
 # Full path to the Xcode project
 PROJECT_PATH="${GITHUB_WORKSPACE}/C++Extension/Projects/xCode/ams_lib.xcodeproj"
 
-# Scheme name - replace 'YourScheme' with your actual scheme name if you know it
-SCHEME="YourScheme"
-
 # Specify the architecture for M1 Mac (arm64)
 ARCH="arm64"
 
@@ -19,7 +16,6 @@ CODE_SIGN_REQUIRED=NO
 # Building the project
 xcodebuild \
   -project "$PROJECT_PATH" \
-  -scheme "$SCHEME" \
   -configuration Release \
   -sdk macosx \
   ARCHS="$ARCH" \
